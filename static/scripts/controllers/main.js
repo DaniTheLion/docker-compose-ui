991f01f921a62d7a21a4664542e400ffb42ca54f
Dani Acherkan <dani.acherkan@naturalint.com>
2015-08-16 22:12:53 +0300

    display project run status in menu

--- static/scripts/controllers/main.js
+++ static/scripts/controllers/main.js
@@ -10,11 +10,18 @@
 angular.module('composeUiApp')
   .controller('MainCtrl', function ($scope, $resource) {
 
-    var Projects = $resource('api/v1/projects');
+    var Projects = $resource('api/v1/projects/:id');
 
     function reload(init) {
       Projects.get(function (data) {
-        $scope.projects = data;
+        $scope.projects = {};
+
+        angular.forEach(data.projects, function(path,name) {
+            Projects.get({id: name}, function(projectData){
+              $scope.projects[name] = { id: name, path: path, containers: projectData.containers };
+            });
+        });
+
         if (!init) {
           alertify.success(Object.keys(data.projects).length + ' projects reloadeded');
         }
@@ -22,6 +29,18 @@ angular.module('composeUiApp')
     }
 
     $scope.reload = reload;
+
+    
+    $scope.isProjectRunning = function (project) {
+      var isRunning = true
+      project.containers.forEach(function(container){
+        if(!container.is_running){
+          isRunning = false;
+        }
+      });
+      return isRunning;
+    };
+
 
     $scope.isEmpty = function (obj) {
       return angular.equals({}, obj);
